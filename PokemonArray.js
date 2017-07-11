73c2011ac03ba2fa6e305b436a09fd5787a72895
keneilASC <keneil.omartin>
2017-07-11 13:42:20 -0400

    first commit

---
+++ PokemonArray.js
@@ -0,0 +1,6 @@
+mypokemonRoster = ["exeggutor","chimchar","chimecho","blastoise","squirtle"];
+var  firstTofight = exeggutor
+var SecondTofight = chimchar 
+for (var i = 0; i < 5 ; i++){
+    console.log(mypokemonRoster[i]);
+}
